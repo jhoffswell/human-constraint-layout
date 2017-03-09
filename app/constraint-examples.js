cdf62a84dc3b0ce2eac38375cebee5f217c129c8
jhoffswell <jhoffs@cs.washington.edu>
2017-03-08 17:22:13 -0800

    Add tlr4 example
    Add tlr4 graph from Cerebral paper

--- app/constraint-examples.js
+++ app/constraint-examples.js
@@ -5,14 +5,16 @@ var EXAMPLES = {
     {"name": "large-tree"},
     {"name": "small-foodWeb"},
     {"name": "large-foodWeb"},
-    {"name": "medium-clusters"}
+    {"name": "medium-clusters"},
+    {"name": "tlr4"}
   ],
   'Graph': [
     {"name": "small-tree"},
     {"name": "medium-tree"},
     {"name": "large-tree"},
     {"name": "small-foodWeb"},
-    {"name": "large-foodWeb"}
+    {"name": "large-foodWeb"},
+    {"name": "tlr4"}
   ],
   'Cola': [
     {"name": "small-alignment"},
